f296de759f6412e068e0264b2430d453fde2acb7
Dhileepguru <dhileepguru497@gmail.com>
2024-08-10 23:06:14 +0530

    Inserted profile

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 
+import { Component } from 'react';
 import './App.css';
 import  Header from './assets/components/Header';
+import Content from './assets/components/Content';
 function App() {
   return (
     <div className="App ">
       <Header />
+      <Content/>
     </div>
     
   );
---
+++ portfolio/src/assets/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Profile from "../images/Profile"
+function Content()
+{
+    return(
+        <section className=" text-red-200">
+        Hi , <br></br>I'm Dhileep D
+        <p>I'm a Software Developer</p>
+        <img src="Profile"></img>
+        </section>
+    )
+}
+
+
+export default Content;

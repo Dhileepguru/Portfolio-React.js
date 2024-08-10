c48899cf407947ace3996d59f7a9770b992299c2
Dhileepguru <dhileepguru497@gmail.com>
2024-08-10 17:23:10 +0530

    Inserted Mobile Nav icon-items

--- portfolio/src/assets/components/Header.js
+++ portfolio/src/assets/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Bars3Icon } from '@heroicons/react/24/solid'
 function Header()
 {
     return(
-    <header className='flex justify-between px-4 my-2 bg text-gray-300 my-2'>
+    
+    <header className='flex justify-between px-4 bg  text-gray-300 '>
         <a className='font-bold' href="#">D Dhileep</a>
         <nav className='hidden md:block'>
         <ul className='flex '>
@@ -14,7 +15,19 @@ function Header()
             <li><a href="#">Contact</a></li>
         </ul>
         </nav>
+        <nav className='block md:hidden'>
+        <ul className='flex flex-col navmob'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </nav>
+        <button className='block md:hidden'><Bars3Icon className='text-white h-5'/></button>
+        
     </header>
+    
 )
   
 }

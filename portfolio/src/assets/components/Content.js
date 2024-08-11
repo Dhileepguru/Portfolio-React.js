14b58f9b30789e3bd5caaf247c8bfe17366825a4
Dhileepguru <dhileepguru497@gmail.com>
2024-08-11 18:03:22 +0530

    Modified Home screen

--- portfolio/src/assets/components/Content.js
+++ portfolio/src/assets/components/Content.js
@@ -5,9 +5,9 @@ import { FaMedium } from "react-icons/fa6";
 function Content()
 {
     return(
-        <section className="bg-black text-gray-100 flex px-5 py-32 justify-center content-style">
+        <section className="bg-black text-gray-100 flex flex-col md:flex-row px-5 md:py-32 justify-center content-style">
         <div className='w-1/2 flex  flex-col'>
-        <h1 className='content  py-40 mx-40 text-6xl '>Hi ,<br/>I'm Dhileep D
+        <h1 className='hidden md:block content  py-40 mx-40 text-6xl '>Hi ,<br/>I'm Dhileep D
             <p className='text-2xl py-2 px-1'>I'm a Software Developer</p>
             <div className='py-3 flex  '>
             <a href='#' className='py-5'><AiOutlineLinkedin/></a>
@@ -15,10 +15,20 @@ function Content()
             <a href='#' className='py-5 ml-4'><FaMedium/></a>
             </div>
          </h1>
-
         </div>
-        
-        <img className='w-1/3 h-1/3 rounded-full ml-auto mx-40 border-4 border-gray-100 ' src={Profile}></img>
+        <div className='md:hidden flex'>
+        <img className=' mt-10 size-1/2 md:block md:w-full h-1/2 rounded-full ml-end  border-4 border-gray-100 ' src={Profile}></img>
+            <h1 className=' block md:hidden content mt-7  py-10 mx-4 text-xl flex flex-col '>Hi ,<br/>I'm Dhileep D
+            <p className='text-sm mt-1 py-2 px-0 '>I'm a Software Developer</p>
+            <div className='py-1 flex   '>
+            <a href='#' className='py-5 ml-end'><AiOutlineLinkedin/></a>
+            <a href='#' className='py-5 ml-4'><AiOutlineGithub/></a>
+            <a href='#' className='py-5 ml-4'><FaMedium/></a>
+            </div>
+            
+            </h1>
+         </div>
+        <img className=' hidden md:block md:w-1/3 h-1/3 rounded-full ml-auto mx-40 border-4 border-gray-100 ' src={Profile}></img>
         </section>
     )
 }
